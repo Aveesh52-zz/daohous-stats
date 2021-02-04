a78dfc30a785dcfaff5b64bfa523873109db4857
Aveesh52 <aveeshshetty007@gmail.com>
2021-02-04 17:07:55 +0530

    error fixed

--- src/factory-mapping.ts
+++ src/factory-mapping.ts
@@ -1,25 +1,15 @@
 import { BigInt, log } from "@graphprotocol/graph-ts";
-import { Register as RegisterV1 } from "../generated/V1Factory/V1Factory";
+
 import {
   Register as RegisterV2,
   Delete,
 } from "../generated/V2Factory/V2Factory";
-import { MolochV1Template, MolochV2Template } from "../generated/templates";
+import { MolochV2Template } from "../generated/templates";
 import { Moloch, DaoMeta } from "../generated/schema";
 import { addSummonBadge, addMembershipBadge, addGas } from "./badges";
 
-export function handleRegisterV1(event: RegisterV1): void {
-  if (event.params.newContract.toString() == "0") {
-    return;
-  }
-  MolochV1Template.create(event.params.moloch);
 
-  let daoMeta = new DaoMeta(event.params.moloch.toHex());
-  daoMeta.title = event.params.title;
-  daoMeta.version = "1";
-  daoMeta.newContract = event.params.newContract.toString();
-  daoMeta.save();
-}
+
 
 export function handleRegisterV2(event: RegisterV2): void {
   MolochV2Template.create(event.params.moloch);
