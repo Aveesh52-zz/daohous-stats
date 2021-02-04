71d455dc6a2c2fbc8848262cbbb0caf985c9da85
Aveesh52 <aveeshshetty007@gmail.com>
2021-02-04 16:37:54 +0530

    v1 files removed

--- generated/templates.ts
+++ generated/templates.ts
@@ -2,12 +2,6 @@
 
 import { Address, DataSourceTemplate } from "@graphprotocol/graph-ts";
 
-export class MolochV1Template extends DataSourceTemplate {
-  static create(address: Address): void {
-    DataSourceTemplate.create("MolochV1Template", [address.toHex()]);
-  }
-}
-
 export class MolochV2Template extends DataSourceTemplate {
   static create(address: Address): void {
     DataSourceTemplate.create("MolochV2Template", [address.toHex()]);
